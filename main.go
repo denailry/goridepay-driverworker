ded55e646d5da2d226ae9b1820c3f445f01e1a57
Daniel Ryan <daniel93ryan@gmail.com>
2018-08-13 17:57:23 +0700

    create order handler for driver's worker

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type order_model struct {
+	Order_id   string
+	Driver_ids []string
+}
+
+type order_response struct {
+	Error   bool
+	Message string
+}
+
+func OrderHandler(w http.ResponseWriter, r *http.Request) {
+	s, _ := ioutil.ReadAll(r.Body)
+	// Put the body back for FormatRequest to read it
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(s))
+	fmt.Printf("--> %s\n\n", r)
+	log.Println(s)
+	decoder := json.NewDecoder(r.Body)
+	var t order_model
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Order ID: " + t.Driver_ids[0])
+	or := order_response{
+		false,
+		"ok",
+	}
+	rj, _ := json.Marshal(or)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(rj)
+}
+
+func main() {
+	port := os.Args[1]
+
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/order", OrderHandler).Methods("POST")
+	http.Handle("/", r)
+	// Add your routes as needed
+
+	srv := &http.Server{
+		Addr: "0.0.0.0:" + port,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		fmt.Println("Listening on port " + port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	fmt.Printf("\b\b")
+	log.Println("shutting down")
+	os.Exit(0)
+}
